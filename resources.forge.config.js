37f9e76269b44aa5e79cc227727cbcdc2003d354
Benjamin Atkin <ben@benatkin.com>
2020-02-27 17:28:04 -0500

    install prettier & reformat

--- resources.forge.config.js
+++ resources.forge.config.js
@@ -1,35 +1,35 @@
 module.exports = {
   packagerConfig: {
     osxSign: {
-      hardenedRuntime: true
+      hardenedRuntime: true,
     },
     osxNotarize: {
-      appBundleId: "com.benatkin.ResourcesAlpha",
-      appPath: "./out/Resources-darwin-x64/Resources.app",
+      appBundleId: 'com.benatkin.ResourcesAlpha',
+      appPath: './out/Resources-darwin-x64/Resources.app',
       appleId: process.env.APPLE_ID,
-      appleIdPassword: process.env.APPLE_ID_PASSWORD
+      appleIdPassword: process.env.APPLE_ID_PASSWORD,
     },
-    appBundleId: "com.benatkin.ResourcesAlpha",
-    appCategoryType: "public.app-category.developer-tools"
+    appBundleId: 'com.benatkin.ResourcesAlpha',
+    appCategoryType: 'public.app-category.developer-tools',
   },
   makers: [
     {
-      name: "@electron-forge/maker-squirrel",
+      name: '@electron-forge/maker-squirrel',
       config: {
-        name: "Resources"
-      }
+        name: 'Resources',
+      },
     },
     {
-      name: "@electron-forge/maker-zip",
-      platforms: ["darwin"]
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
     },
     {
-      name: "@electron-forge/maker-deb",
-      config: {}
+      name: '@electron-forge/maker-deb',
+      config: {},
     },
     {
-      name: "@electron-forge/maker-rpm",
-      config: {}
-    }
-  ]
-};
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+}
